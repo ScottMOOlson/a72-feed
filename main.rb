e3381abc74217f18b743d628733dd363ec0b2c51
ScottMOOlson <scottmoolson@gmail.com>
2025-07-19 15:29:48 -0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,13 +2,13 @@ require 'json'
 require 'open-uri'
 require_relative 'a72_feed'
 
-district_index = 12
-district_name = 'District 10'
+district_id = 5647  # For District 16
 
-ps_cso_url = ENV.fetch('PS_CSO_URL')
-data = URI.open(ps_cso_url).read
+a72_url = ENV.fetch('A72_URL')
+data = URI.open(a72_url).read
 json = JSON.parse(data)
-meetings = json['values'].
-  select {|v| v[district_index] == district_name }.
-  map {|m| A72Feed.google_meeting_to_tsml(m) }
-puts JSON.dump(meetings)
+
+meetings = json.select { |m| m['district_id'] == district_id 
+               .map { |m| A72Feed.google_meeting_to_tsml(m) }
+
+puts JSON.pretty_generate(meetings)
